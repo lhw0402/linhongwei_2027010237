f9fe45ec4b0b66028930b80ea72ffd99768948c3
linhongwei <2246711071@qq.com>
2023-05-19 23:01:35 +0800

    第八次作业

---
+++ Image/Image/ImageDlg.cpp
@@ -0,0 +1,242 @@
+﻿
+// ImageDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Image.h"
+#include "ImageDlg.h"
+#include "afxdialogex.h"
+#include <string>
+#include <afxwin.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CImageDlg 对话框
+
+
+
+CImageDlg::CImageDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_IMAGE_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CImageDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CImageDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON1, &CImageDlg::OnBnClickedButtonSingle)
+	ON_BN_CLICKED(IDC_BUTTON2, &CImageDlg::OnBnClickedButtonDir)
+	ON_EN_CHANGE(IDC_EDIT_INPUT, &CImageDlg::OnEnChangeEditInput)
+	ON_BN_CLICKED(IDC_BUTTON3, &CImageDlg::OnBnClickedButton3)
+	ON_EN_CHANGE(IDC_EDIT3, &CImageDlg::OnEnChangeEdit3)
+END_MESSAGE_MAP()
+
+
+// CImageDlg 消息处理程序
+
+BOOL CImageDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CImageDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CImageDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CImageDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CImageDlg::OnBnClickedButtonSingle()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CString OpenFilter = _T("图片文件(*.*)|*.*|");
+
+	CFileDialog FileOpen(TRUE, NULL, NULL, OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT, OpenFilter);
+	if (IDOK == FileOpen.DoModal())
+	{
+		CString PathName = FileOpen.GetPathName();
+		GetDlgItem(IDC_EDIT_INPUT)->SetWindowTextW(PathName);
+		GetDlgItem(IDC_EDIT_INPUT)->SetFont(&m_oFont);
+	}
+}
+
+
+void CImageDlg::OnBnClickedButtonDir()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	TCHAR szBuffer[MAX_PATH] = { 0 };
+	BROWSEINFO bi;
+	ZeroMemory(&bi, sizeof(BROWSEINFO));
+	bi.hwndOwner = NULL;
+	bi.pszDisplayName = szBuffer;
+	bi.lpszTitle = _T("从下面选文件夹目录:");
+	bi.ulFlags = BIF_RETURNFSANCESTORS; //BIF_BROWSEINCLUDEFILES
+	LPITEMIDLIST idl = SHBrowseForFolder(&bi);
+	if (NULL == idl)
+	{
+		return;
+	}
+	SHGetPathFromIDList(idl, szBuffer);
+	if (szBuffer)
+	{
+		std::wstring strFilePath = szBuffer;
+
+		//文本框写入
+		CEdit* pEditSourceFileDir = (CEdit*)GetDlgItem(IDC_EDIT_INPUT);
+		if (pEditSourceFileDir)
+		{
+			pEditSourceFileDir->SetWindowText(szBuffer);
+			pEditSourceFileDir->SetFont(&m_oFont);
+		}
+	}
+}
+
+
+void CImageDlg::OnEnChangeEditInput()
+{
+	// TODO:  如果该控件是 RICHEDIT 控件，它将不
+	// 发送此通知，除非重写 CDialogEx::OnInitDialog()
+	// 函数并调用 CRichEditCtrl().SetEventMask()，
+	// 同时将 ENM_CHANGE 标志“或”运算到掩码中。
+
+	// TODO:  在此添加控件通知处理程序代码
+}
+
+
+void CImageDlg::OnBnClickedButton3()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CString  strSourceDir;
+	CEdit* pEditSourceDir = (CEdit*)GetDlgItem(IDC_EDIT_INPUT);
+	if (pEditSourceDir)
+	{
+		pEditSourceDir->GetWindowText(strSourceDir);
+	}
+}
+
+
+void CImageDlg::OnEnChangeEdit3()
+{
+	// TODO:  如果该控件是 RICHEDIT 控件，它将不
+	// 发送此通知，除非重写 CDialogEx::OnInitDialog()
+	// 函数并调用 CRichEditCtrl().SetEventMask()，
+	// 同时将 ENM_CHANGE 标志“或”运算到掩码中。
+
+	// TODO:  在此添加控件通知处理程序代码
+}
---
+++ Image/Image/ImageDlg.h
@@ -0,0 +1,44 @@
+﻿
+// ImageDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CImageDlg 对话框
+class CImageDlg : public CDialogEx
+{
+// 构造
+public:
+	CImageDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_IMAGE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButtonSingle();
+	afx_msg void OnBnClickedButtonDir();
+	afx_msg void OnEnChangeEditInput();
+	afx_msg void OnBnClickedButton3();
+private:
+	CFont m_oFont;
+	
+public:
+	afx_msg void OnEnChangeEdit3();
+};
