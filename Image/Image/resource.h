6c79224be4a46e5d437d4eef62a197d7cb531509
linhongwei <2246711071@qq.com>
2023-06-09 09:04:24 +0800

    作业11

--- Image/Image/resource.h
+++ Image/Image/resource.h
@@ -12,6 +12,7 @@
 #define IDC_EDIT_INPUT                  1002
 #define IDC_EDIT3                       1005
 #define IDC_BUTTON3                     1006
+#define IDC_BUTTON4                     1007
 
 // Next default values for new objects
 // 
@@ -19,7 +20,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
